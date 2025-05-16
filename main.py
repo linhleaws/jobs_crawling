8d61dbc65720f4fc7c4248c2aef0fb520ebfab01
Linh Le <linhle@Linhs-MacBook-Pro.local>
2025-05-16 17:16:05 +0700

    change logic

--- main.py
+++ main.py
@@ -21,39 +21,13 @@ def main():
         if df.shape[0] == 1:
             message = f"""
             No results found. Something went wrong.
-            No jobs were found for the given search criteria.
-            Please consider the following:
-            1. Try adjusting your search criteria.
-            2. If you used English search keywords for non-English speaking countries,
-               it might return an empty result. Consider using keywords in the country's language.
-            3. Try more general keyword(s), check your spelling or replace abbreviations with the entire word
-
-            Feel free to try a manual search with this link and see for yourself:
-            Link {full_url}
+            Check the URL: {full_url}
             """
             send_message_slack(message)
-
-            # subject = 'No Jobs Found on Indeed'
-            # body = """
-            # No jobs were found for the given search criteria.
-            # Please consider the following:
-            # 1. Try adjusting your search criteria.
-            # 2. If you used English search keywords for non-English speaking countries,
-            #    it might return an empty result. Consider using keywords in the country's language.
-            # 3. Try more general keyword(s), check your spelling or replace abbreviations with the entire word
-
-            # Feel free to try a manual search with this link and see for yourself:
-            # Link {}
-            # """.format(full_url)
-
-            # send_email_empty(sender_email, receiver_email, subject, body, password)
-            print(df)
         else:
             cleaned_df = clean_data(df)
-            # csv_file = save_csv(cleaned_df, job_position, job_location)
     finally:
         try:
-            # send_email(cleaned_df, sender_email, receiver_email, job_position, job_location, password)
             write_to_s3(cleaned_df, job_position, job_location)
         except Exception as e:
             message = f"Error saving file: {e}"
